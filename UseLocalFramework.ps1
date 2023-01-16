c0ced144a01d898f6ba45e9d3cbef2ae1a4e7879
Caiyi Hsu <cai1hsu@outlook.com>
2023-01-16 23:06:37 +0800

    Update UseLocalFramework.ps1

--- UseLocalFramework.ps1
+++ UseLocalFramework.ps1
@@ -7,7 +7,7 @@ $CSPROJ="osu.Game/osu.Game.csproj"
 $SLN="osu.sln"
 
 dotnet remove $CSPROJ package ppy.osu.Framework;
-dotnet sln $SLN add ../osu-framework/osu.Framework/osu.Framework.csproj ../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj;
+dotnet sln $SLN add ../osu-framework/osu.Framework/osu.Framework.csproj ../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj
 dotnet add $CSPROJ reference ../osu-framework/osu.Framework/osu.Framework.csproj
 
 $SLNF=Get-Content "osu.Desktop.slnf" | ConvertFrom-Json
